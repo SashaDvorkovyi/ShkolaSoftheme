1d374c274175b222d7b9ea59ba99f9638c943132
DVORKOVIJ-ALEX\sdo <sasha3489@ukr.net>
2018-02-13 12:46:28 +0300

    no message

--- CSharpHW/lesson 9/CarConstruct/CarConstruct/Program.cs
+++ CSharpHW/lesson 9/CarConstruct/CarConstruct/Program.cs
@@ -11,13 +11,14 @@ namespace CarConstruct
         static void Main(string[] args)
         {
 
-
             var car1 = CarConstructor.Construct(Color.black, Engine.М_271, Transmission.mechanics);
 
-            Console.WriteLine("Car1: " + car1.color.ToString()+ ", " + car1.engine.ToString() + ", " + car1.transmission.ToString());
-            car1 = CarConstructor.Reconstruct(car1, Engine.М_272);
             Console.WriteLine("Car1: " + car1.color.ToString() + ", " + car1.engine.ToString() + ", " + car1.transmission.ToString());
-            car1 = CarConstructor.Reconstruct(car1,Color.red,Engine.ОМ_611);
+            car1 = CarConstructor.Reconstruct(car1, Engine.М_272);
+
+            Console.WriteLine("Car1: " + car1.color.ToString() + ", " + car1.engine.ToString() + ", " + car1.transmission.ToString());
+            car1 = CarConstructor.Reconstruct(car1, Color.red, Engine.ОМ_611);
+
             Console.WriteLine("Car1: " + car1.color.ToString() + ", " + car1.engine.ToString() + ", " + car1.transmission.ToString());
             Console.ReadKey();
         }
