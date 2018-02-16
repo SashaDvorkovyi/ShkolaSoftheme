91dd77731d6fc6447d160de78b0d09a6756bafd8
DVORKOVIJ-ALEX\sdo <sasha3489@ukr.net>
2018-02-16 15:27:55 +0300

    no message

---
+++ CSharpHW/lesson 10/MyArrey/MyArrey/List.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyArrey
+{
+    public class List<T>
+    {
+        private T[] _arrey;
+        private int _lastElement=default(int);
+        private int _lenght = 10;
+
+        public List()
+        {
+            _arrey = new T[_lenght];
+        }
+        private List(int _lenght)
+        {
+            _arrey = new T[_lenght];
+        }
+
+        public void Add(T item)
+        {
+            if (_lenght == _lastElement)
+            {
+                _lenght *= 2;
+                List<T> newLisr= new List<T>(_lenght);
+                for(var i=0; i<this._lastElement; i++)
+                {
+                    newLisr._arrey[i] = this._arrey[i];
+                }
+                this._arrey = newLisr._arrey;
+            }
+            _arrey[_lastElement] = item;
+            _lastElement++;
+        }
+
+        public int Lenght()
+        {
+            return _lastElement;
+        }
+        public T GetByIndex(int index)
+        {
+            if (index > _lastElement - 1)
+            {
+                return default(T);
+            }
+            return _arrey[index-1];
+        }
+
+        public bool Contains(T item)
+        {
+            bool answer = false;
+            for(int i=0; i<_lastElement; i++)
+            {
+                if ( string.Compare( _arrey[i].ToString(), item.ToString())==0)
+                {
+                    answer = true;
+                }
+            }
+            return answer;
+        }
+        public void Show()
+        {
+            for (var i = 0; i < _lastElement; i++)
+            {
+                Console.Write(_arrey[i] + ", ");
+            }
+        }
+    }
+}
---
+++ CSharpHW/lesson 10/MyArrey/MyArrey/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyArrey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            for(var i=0; i<50; i++)
+            {
+                list.Add(i);
+            }
+
+            list.Show();
+
+            Console.WriteLine();
+            Console.WriteLine(list.Contains(40));
+            Console.WriteLine(list.Lenght());
+            Console.WriteLine(list.GetByIndex(20));
+
+            Console.ReadKey();
+
+        }
+    }
+}
