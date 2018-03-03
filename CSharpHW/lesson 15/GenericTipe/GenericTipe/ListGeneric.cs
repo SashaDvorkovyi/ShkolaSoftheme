65c1ea31076fdb52cc0047dc44c33ef2c6016294
DVORKOVIJ-ALEX\sdo <sasha3489@ukr.net>
2018-03-03 14:43:56 +0300

    no message

---
+++ CSharpHW/lesson 15/GenericTipe/GenericTipe/ListGeneric.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericTipe
+{
+    class ListGeneric<T>
+    {
+        private T[] _array;
+        private int _lenght;
+        private const int _AddLenght= 10;
+
+        public ListGeneric(params T[] element)
+        {
+            _array = new T[element.Length+ _AddLenght];
+            _lenght = element.Length;
+            for (var i = 0; i < element.Length; i++)
+            {
+                _array[i] = element[i];
+            }
+        }
+
+        public void AddElement(params T[] element)
+        {
+            if (_lenght + element.Length > _array.Length)
+            {
+                var nawArray = new T[element.Length + _lenght+ _AddLenght];
+                Array.Copy(_array, nawArray, _array.Length);
+                _array = nawArray;
+                var a = default(int);
+                for (var i = _lenght; i < element.Length + _lenght; i++)
+                {
+                    _array[i] = element[a++];
+                }
+                _lenght += element.Length;
+            }
+            else
+            {
+                var a = default(int);
+                for (var i = _lenght; i < element.Length + _lenght; i++)
+                {
+                    _array[i] = element[a++];
+                }
+                _lenght += element.Length;
+            }
+
+        }
+        public void DeleteElement(int elementNumber)
+        {
+            if (0 < elementNumber && elementNumber < _lenght)
+            {
+                for(var i=elementNumber; i<_lenght; i++)
+                {
+                    _array[i - 1] = _array[i];
+                }
+                _lenght--;
+            }
+        }
+        public void DeleteElement(T obj)
+        {
+            for(var i =0; i<_lenght; i++)
+            {
+                if (obj.Equals(_array[i]))
+                {
+                    for (var t = i; t < _lenght; t++)
+                    {
+                        _array[t - 1] = _array[t];
+                    }
+                    _lenght--;
+                    break;
+                }
+            }
+        }
+        public int Lenght() => _lenght;
+
+        public bool Equals(T obj)
+        {
+            var resalt = default(bool);
+            foreach(var item in _array)
+            {
+                if (obj.Equals(item))
+                {
+                    resalt = true;
+                    break;
+                }
+            }
+            return resalt;
+        }
+
+        public T[] ToArray()
+        {
+            if (_array.Length != _lenght)
+            {
+                var newArray = new T[_lenght];
+                Array.Copy(_array, newArray, _lenght);
+                return newArray;
+            }
+            else
+            {
+                return _array;
+            }
+        }
+    }
+}
---
+++ CSharpHW/lesson 15/GenericTipe/GenericTipe/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericTipe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            var obj = new ListGeneric<int>(array);
+            obj.AddElement(11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21);
+            Console.WriteLine(obj.Lenght());
+            obj.DeleteElement(10);
+            Console.WriteLine(obj.Lenght());
+            obj.DeleteElement(10);
+            Console.WriteLine(obj.Lenght());
+            Console.WriteLine(obj.Equals(11));
+            int[] array2 = obj.ToArray();
+            Console.WriteLine();
+            foreach(var item in array2)
+            {
+                Console.Write(item + ", ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
