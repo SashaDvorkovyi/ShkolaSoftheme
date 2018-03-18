9c776c79c4d188a415c6e31ccac9ebf39431930c
WIN-H2UI3UGNKST\User <sasha3489@ukr.net>
2018-03-18 20:03:01 +0200

    no message

---
+++ CSharpHW/lesson 20/Mobile_operator/Mobile_operator/DataCallEndMessage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mobile_operator
+{
+    class DataCallEndMessage
+    {
+        public int OutCall { get; set; }
+        public int OutMessage { get; set; }
+        public int InCall { get; set; }
+        public int InMessage { get; set; }
+
+    }
+}
---
+++ CSharpHW/lesson 20/Mobile_operator/Mobile_operator/MobileOperator.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Mobile_operator
+{
+    class MobileOperator
+    {
+        private Dictionary<int, MobileAccount> _dictAccount;
+        private Dictionary<int, DataCallEndMessage> _magazine;
+
+        public MobileOperator()
+        {
+            _dictAccount = new Dictionary<int, MobileAccount>();
+            _magazine = new Dictionary<int, DataCallEndMessage>();
+        }
+
+        public bool AddAAccount(MobileAccount account)
+        {
+            var result = default(bool);
+            var count = _dictAccount.Count;
+            _dictAccount.Add(account.Number, account);
+            if (count != _dictAccount.Count)
+            {
+                _magazine.Add(account.Number, new DataCallEndMessage());
+                _dictAccount[account.Number].MessageEvent += AcceptAndSend;
+                _dictAccount[account.Number].CallEvent += AcceptAndSend;
+                result = true;
+            }
+            return result;
+        }
+
+        public void DeleteAccount(int number)
+        {
+            if (_dictAccount.ContainsKey(number))
+            {
+                _dictAccount.Remove(number);
+                _magazine.Remove(number);
+                _dictAccount[number].MessageEvent -= AcceptAndSend;
+                _dictAccount[number].CallEvent -= AcceptAndSend;
+
+            }
+        }
+
+        public MobileAccount TakeAccount(int number) => _dictAccount[number];
+
+        public void AcceptAndSend(object accountOut, EEventArgs eventArg)
+        {
+            var account = accountOut as MobileAccount;
+            if (account != null)
+            {
+                if (_dictAccount.ContainsKey(eventArg.Number))
+                {
+                    if (eventArg.Message == null)
+                    {
+                        _magazine[eventArg.Number].InCall += 2;
+                        _magazine[account.Number].OutCall += 2;
+                        _dictAccount[eventArg.Number].Show(account, eventArg);
+                    }
+                    else
+                    {
+                        _magazine[eventArg.Number].InCall++;
+                        _magazine[account.Number].OutCall++;
+                        _dictAccount[eventArg.Number].Show(account, eventArg);
+                    }
+                }
+            }
+ 
+        }
+
+        public void Top_5_Outgoing()
+        {
+            var result = _magazine.OrderBy(x =>- x.Value.OutCall - x.Value.OutMessage).Take(5);
+            foreach(var item in result)
+            {
+                Console.WriteLine(item.Key);
+            }
+        }
+
+        public void Top_5_Ingoing()
+        {
+            var result = _magazine.OrderBy(x => -x.Value.InCall - x.Value.InMessage).Take(5);
+            foreach (var item in result)
+            {
+                Console.WriteLine(item.Key);
+            }
+        }
+    }
+}
