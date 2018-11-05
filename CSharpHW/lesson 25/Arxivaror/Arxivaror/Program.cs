4814fe5948055fe90dab4156f7a8c43286e8e63d
DVORKOVIJ-ALEX\sdo <sasha3489@ukr.net>
2018-11-05 14:51:09 +0200

    no message

--- CSharpHW/lesson 25/Arxivaror/Arxivaror/Program.cs
+++ CSharpHW/lesson 25/Arxivaror/Arxivaror/Program.cs
@@ -16,16 +16,48 @@ namespace Arxivaror
         {
             Console.WriteLine("Pleas enter the all name of directiry");
             var directiry = new DirectoryInfo(Console.ReadLine());
-            var listOfFiles = GeaAllFiles(directiry);
+            Console.WriteLine(@"Pleas enter 'r' if you want want to archive files, or 'z' if you want extract from archive");
+            var key = Console.ReadKey();
+            if (key.KeyChar == 'r')
+            {
+                try
+                {
+                    var listOfFiles = GetAllFilesWithOutZip(directiry);
+                    WriteToZipInThreeThrids(listOfFiles);
+                }
+                catch (DirectoryNotFoundException)
+                {
+                    Console.WriteLine("You have entered the wrong folder name");
+                }
 
+            }
+            else if (key.KeyChar == 'z')
+            {
+                try
+                {
+                    var listOfZip = GetAllZipFile(directiry);
+                    UnZipInThreeThrids(listOfZip);
+                }
+                catch (DirectoryNotFoundException)
+                {
+                    Console.WriteLine("You have entered the wrong folder name");
+                }
+            }
 
+            Console.WriteLine();
+            Console.WriteLine("Already done. You can close the program.");
+            Console.ReadKey();
+        }
+
+        public static void WriteToZipInThreeThrids(List<string> listOfFiles)
+        {
             Thread[] treads = new Thread[] {
-                new Thread(new ParameterizedThreadStart(ReadAndWriteToZip)),
-                new Thread(new ParameterizedThreadStart(ReadAndWriteToZip)),
-                new Thread(new ParameterizedThreadStart(ReadAndWriteToZip))
+                new Thread(new ParameterizedThreadStart(WriteToZip)),
+                new Thread(new ParameterizedThreadStart(WriteToZip)),
+                new Thread(new ParameterizedThreadStart(WriteToZip))
             };
 
-            for (var fileNumeNumber=0; fileNumeNumber< listOfFiles.Count; fileNumeNumber++)
+            for (var fileNumeNumber = 0; fileNumeNumber < listOfFiles.Count; fileNumeNumber++)
             {
                 if (fileNumeNumber < treads.Length)
                 {
@@ -33,29 +65,57 @@ namespace Arxivaror
                     continue;
                 }
                 var i = 0;
-                while(true)
+                while (true)
                 {
-                    if(string.Equals(treads[i].ThreadState.ToString(), "Stopped"))
+                    if (string.Equals(treads[i].ThreadState.ToString(), "Stopped"))
                     {
-                        treads[i] = new Thread(new ParameterizedThreadStart(ReadAndWriteToZip));
+                        treads[i] = new Thread(new ParameterizedThreadStart(WriteToZip));
                         treads[i].Start(listOfFiles[fileNumeNumber]);
                         break;
                     }
-                    i++;
-                    if (i == 3)
-                    { i = 0; }
+                    i = (i < 3) ? i++ : i = 0;
                 }
-
             }
-            foreach(var thread in treads)
+            foreach (var thread in treads)
             {
                 thread.Join();
             }
-            Console.WriteLine("Already done. You can close the program.");
-            Console.ReadKey();
         }
 
-        public static void ReadAndWriteToZip(object fileNameObj)
+        public static void UnZipInThreeThrids(List<string> listOfZip)
+        {
+            Thread[] treads = new Thread[] {
+                new Thread(new ParameterizedThreadStart(UnZip)),
+                new Thread(new ParameterizedThreadStart(UnZip)),
+                new Thread(new ParameterizedThreadStart(UnZip))
+            };
+
+            for (var fileNumeNumber = 0; fileNumeNumber < listOfZip.Count; fileNumeNumber++)
+            {
+                if (fileNumeNumber < treads.Length)
+                {
+                    treads[fileNumeNumber].Start(listOfZip[fileNumeNumber]);
+                    continue;
+                }
+                var i = 0;
+                while (true)
+                {
+                    if (string.Equals(treads[i].ThreadState.ToString(), "Stopped"))
+                    {
+                        treads[i] = new Thread(new ParameterizedThreadStart(UnZip));
+                        treads[i].Start(listOfZip[fileNumeNumber]);
+                        break;
+                    }
+                    i = (i < 3) ? i++ : i = 0;
+                }
+            }
+            foreach (var thread in treads)
+            {
+                thread.Join();
+            }
+        }
+
+        public static void WriteToZip(object fileNameObj)
         {
             var fileName = fileNameObj.ToString();
             using (FileStream fs = new FileStream(fileName.Substring(0, fileName.LastIndexOf('.')) + ".zip", FileMode.OpenOrCreate))
@@ -67,9 +127,26 @@ namespace Arxivaror
             }
         }
 
+        public static void UnZip(object fileNameObj)
+        {
+            var fileName = fileNameObj.ToString();
+            using (FileStream fs = new FileStream(fileName, FileMode.Open))
+            {
+                try
+                {
+                    using (ZipArchive arch = new ZipArchive(fs))
+                    {
+                        arch.ExtractToDirectory(fileName.Substring(0, fileName.LastIndexOf("\\")));
+                    }
+                }
+                catch (IOException)
+                {
+                    Console.WriteLine("A file with that name \"{0}\" already exists!", fileName);
+                }
+            }
+        }
 
-
-        public static List<string> GeaAllFiles(DirectoryInfo directiry)
+        public static List<string> GetAllFilesWithOutZip(DirectoryInfo directiry)
         {
             var list = new List<string>();
             foreach (var file in directiry.GetFiles("*", SearchOption.AllDirectories))
@@ -78,6 +155,16 @@ namespace Arxivaror
                 {
                     continue;
                 }
+                list.Add(file.FullName);
+            }
+            return list;
+        }
+
+        public static List<string> GetAllZipFile(DirectoryInfo directiry)
+        {
+            var list = new List<string>();
+            foreach (var file in directiry.GetFiles("*.zip", SearchOption.AllDirectories))
+            {
                 list.Add(file.FullName);
             }
             return list;
